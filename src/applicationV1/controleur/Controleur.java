e0f70da5eb89282c73cf92f88bafe8fb0b84e3df
Darina-Codjo <darina.codjo@gmail.com>
2020-05-11 14:41:23 +0200

    essaie afficher img

--- src/applicationV1/controleur/Controleur.java
+++ src/applicationV1/controleur/Controleur.java
@@ -1,5 +1,31 @@
 package applicationV1.controleur;
 
-public class Controleur {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import applicationV1.modele.Terrain;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.TilePane;
+
+public class Controleur implements Initializable{
+	
+	private Terrain monTerrain;
+	    @FXML
+	    private TilePane TilePane;
+
+		@Override
+		public void initialize(URL location, ResourceBundle resources) {
+			this.monTerrain = new Terrain();
+			afficherTerrain();
+		}
+
+		private int afficherTerrain() {
+			int codeTuile = 0;
+			for(int i = 0; i < 15*15; i++) {
+				codeTuile = monTerrain.getNumeroTuile(i);
+			}
+			return codeTuile;
+		}
 
 }
--- src/applicationV1/modele/Terrain.java
+++ src/applicationV1/modele/Terrain.java
@@ -1,41 +1,39 @@
 package applicationV1.modele;
 
-import java.util.ArrayList;
-
 public class Terrain {
-	private int width;
-	private int height;
-	private ArrayList<Acteur> acteurs;
-	private int[][] tab;
+	private int width = 15;
+	private int height = 15;
 
-	public Terrain(int width, int height) {
-		this.width = width;
-		this.height = height;
-		this.acteurs = new ArrayList<Acteur>();
-		this.tab = new int[15][15];
-	}
+	private int[] map= {256, 257, 258, 259, 5, 5, 5, 5, 5, 5, 5, 5, 5, 179, 178,
+						260, 261, 262, 263, 1, 1, 97, 1, 87, 94, 94, 94, 88, 177, 
+						174, 3, 264, 265, 1, 1, 1, 1, 1, 11, 92, 93, 13, 86, 173, 
+						180, 3, 1, 1, 1, 1, 1, 1, 1, 11, 86, 1, 11, 12, 176, 172, 
+						3, 97, 1, 1, 87, 94, 94, 94, 91, 86, 1, 14, 93, 175, 171, 
+						3, 1, 1, 1, 11, 92, 93, 93, 93, 89, 181, 182, 1, 1, 2, 3, 
+						1, 1, 1, 11, 86, 1, 98, 1, 1, 1, 1, 1, 1, 2, 3, 1, 1, 1, 
+						11, 12, 94, 94, 94, 94, 94, 88, 1, 1, 2, 90, 94, 94, 88, 
+						14, 93, 93, 93, 93, 93, 13, 86, 1, 1, 2, 95, 93, 13, 86, 
+						1, 1, 1, 1, 1, 1, 11, 86, 1, 1, 10, 3, 184, 11, 86, 1, 1, 
+						1, 1, 1, 1, 11, 86, 1, 1, 2, 3, 1, 11, 86, 96, 1, 1, 1, 1,
+						1, 11, 86, 1, 1, 2, 3, 1, 11, 12, 94, 94, 94, 94, 94, 94, 
+						91, 86, 1, 1, 2, 3, 1, 14, 93, 93, 93, 93, 93, 93, 93, 93, 
+						89, 1, 97, 2, 9, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 8};
 	
+	private int[][] grille;
+	
+
+//	public Terrain(int width, int height) {
+//		this.width = width;
+//		this.height = height;
+//	}
 	public int getWidth() {
 		return width;
 	}
-
 	public int getHeight() {
 		return height;
 	}
-
-	public ArrayList<Acteur> getActeurs() {
-		return acteurs;
-	}
 	
-	public Acteur getActeur(String id) {
-		
-	for(Acteur acteur : this.acteurs){
-		if(acteur.getId().equals(id))return acteur;
-	}
-	return null;
-	}
-	
-	public void ajouter(Acteur acteur){
-		acteurs.add(acteur);
+	public int getNumeroTuile(int indice) {
+		return map[indice]; //TODO coordonnées en X Y %15 ou tab[][] pour faire la grille
 	}
 }
