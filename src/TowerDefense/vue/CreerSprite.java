caf9aec87a5f11392be856082f3a4f64d8cb457d
Adrien Palerm <adrien.palerm33@gmail.com>
2020-06-03 10:48:37 +0200

    Ajouté les vrais sprites des tourelles

--- src/TowerDefense/vue/CreerSprite.java
+++ src/TowerDefense/vue/CreerSprite.java
@@ -8,6 +8,7 @@ import TowerDefense.modele.Cactus;
 import TowerDefense.modele.ScorpionSpeciale;
 import TowerDefense.modele.Serpent;
 import TowerDefense.modele.SerpentSpeciale;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -21,27 +22,27 @@ public class CreerSprite {
 	}
 	
 	// en fonction du type d'ennemi, créer pour l'instant des sprites sous forme de cercle de différentes couleurs par ennemi
-	public void acteurSprite(Acteur acteur) {
-		Circle circle = new Circle(3);
-		
-		if(acteur instanceof Cactus)
-			circle.setFill(Color.LIGHTGREEN);
-			//Color.web("color en hexa")
-		else if (acteur instanceof Scorpion) 
-			circle.setFill(Color.BROWN);
-		else if (acteur instanceof Serpent) 
-			circle.setFill(Color.ORANGE);
-		else if (acteur instanceof ScorpionSpeciale)
-			circle.setFill(Color.RED);
-		else if (acteur instanceof SerpentSpeciale)
-			circle.setFill(Color.GREEN);
-		else
-			circle.setFill(Color.PURPLE);
-		
-		circle.setId(acteur.getId());
-		circle.setTranslateX(acteur.getX());
-		circle.setTranslateY(acteur.getY());
-		plateau.getChildren().add(circle);
+	public void acteurSprite(Acteur acteur, double x, double y) {
+		ImageView texture;
+		if (acteur instanceof TourelleGlace ) {
+			texture = new ImageView("textures/tourelleglace.png");
+			texture.setFitWidth(16);
+			texture.setPreserveRatio(true);
+		}
+		else if (acteur instanceof TourelleFeu) {
+			texture = new ImageView("textures/tourellefeu.png");
+			texture.setFitWidth(16);
+			texture.setPreserveRatio(true);
+		}
+		else {
+			texture = new ImageView("textures/tourelleroche.png");
+			texture.setFitWidth(16);
+			texture.setPreserveRatio(true);
+		}
+	
+		texture.setTranslateX(x);
+		texture.setTranslateY(y);
+		plateau.getChildren().add(texture);
 	}
 	
 	
