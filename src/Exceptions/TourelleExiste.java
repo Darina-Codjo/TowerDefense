5cf3529fa28ca7b3ae82de3972ca6d16eca287fd
doniamef <donia.mef@gmail.com>
2020-06-11 16:06:32 +0200

    séparation de bouton  start et restart

--- src/Exceptions/TourelleExiste.java
+++ src/Exceptions/TourelleExiste.java
@@ -5,31 +5,31 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 
 public class TourelleExiste {
- private Jeu jeu ;
+	private Jeu jeu ;
 
-public TourelleExiste(Jeu jeu) {
-	this.jeu = jeu;
-}
-public TourelleExiste() {
- 
-}
-
-public static void tourellsPlus0()throws InexistantException {
-	try {
-		if( Jeu.listeTourelle().size()==0)
-			throw new InexistantException();
-		
-		
-	}catch ( InexistantException i) {
-		Alert alert= new Alert(AlertType.INFORMATION);
-		alert.setTitle("Tourelle");
-		alert.setHeaderText(null);
-		alert.setContentText("veuillez mettre au moins une Tourelle!");
-		
-		alert.showAndWait();
-		
-		throw i;
-		
+	public TourelleExiste(Jeu jeu) {
+		this.jeu = jeu;
 	}
-}
+	public TourelleExiste() {
+
+	}
+
+	public static void tourellsPlus0()throws InexistantException {
+		try {
+			if( Jeu.listeTourelle().size()==0)
+				throw new InexistantException();
+
+
+		}catch ( InexistantException i) {
+			Alert alert= new Alert(AlertType.INFORMATION);
+			alert.setTitle("Tourelle");
+			alert.setHeaderText(null);
+			alert.setContentText("veuillez mettre au moins une Tourelle!");
+
+			alert.showAndWait();
+
+			throw i;
+
+		}
+	}
 }
