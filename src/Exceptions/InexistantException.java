6f8767996629d5beb8adf6a33189cf22b0d002f4
doniamef <donia.mef@gmail.com>
2020-06-11 15:57:41 +0200

    Rajouter un package exception pour empêcher le joueur de lancer une partie si il na pas au moins poser 1 tourelle

---
+++ src/Exceptions/InexistantException.java
@@ -0,0 +1,5 @@
+package Exceptions;
+
+public class InexistantException extends Exception {
+
+}
---
+++ src/Exceptions/TourelleExiste.java
@@ -0,0 +1,35 @@
+package Exceptions;
+
+import TowerDefense.modele.Jeu;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+public class TourelleExiste {
+ private Jeu jeu ;
+
+public TourelleExiste(Jeu jeu) {
+	this.jeu = jeu;
+}
+public TourelleExiste() {
+ 
+}
+
+public static void tourellsPlus0()throws InexistantException {
+	try {
+		if( Jeu.listeTourelle().size()==0)
+			throw new InexistantException();
+		
+		
+	}catch ( InexistantException i) {
+		Alert alert= new Alert(AlertType.INFORMATION);
+		alert.setTitle("Tourelle");
+		alert.setHeaderText(null);
+		alert.setContentText("veuillez mettre au moins une Tourelle!");
+		
+		alert.showAndWait();
+		
+		throw i;
+		
+	}
+}
+}
